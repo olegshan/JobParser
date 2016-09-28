66848028c0af0aec58fd3cd0b655b22f45b604b5
Oleg Shankovskyi <olegshan@gmail.com>
2016-09-28 23:10:13 +0300

    HeadHunter added

---
+++ src/main/java/com/olegshan/service/HeadHunterService.java
@@ -0,0 +1,63 @@
+package com.olegshan.service;
+
+import com.olegshan.entity.Job;
+import com.olegshan.tools.MonthsTools;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by olegshan on 28.09.2016.
+ */
+public class HeadHunterService {
+
+    public static List<Job> getJobs() throws IOException {
+
+        List<Job> jobs = new ArrayList<>();
+
+        Document doc = Jsoup.connect("https://hh.ua/search/vacancy?text=java&area=115").userAgent("Mozilla").get();
+
+        Elements jobBlocks = doc.getElementsByAttributeValue("class", "search-result-description");
+        jobBlocks.forEach(job -> {
+            Elements titleBlock = job.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-title");
+            String url = titleBlock.attr("href");
+            String title = titleBlock.text();
+            String description = job.
+                    getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy_snippet_requirement").text();
+            String company = job.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-employer").text();
+            String source = "HeadHunter";
+            String dateLine = job.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-date").text();
+            Date date = getDate(dateLine);
+
+            Job rabotaJob = new Job(title, description, company, source, url, date);
+            jobs.add(rabotaJob);
+
+        });
+        jobs.forEach(System.out::println);
+        return jobs;
+    }
+
+    private static Date getDate(String line) {
+        String[] dateParts;
+        int year;
+        int month;
+        int day;
+        dateParts = line.split("\u00a0");
+        day = Integer.parseInt(dateParts[0]);
+        year = new Date().getYear();
+
+        month = MonthsTools.MONTHS.get(dateParts[1].toLowerCase());
+
+        return new Date(year, month, day);
+    }
+
+    public static void main(String[] args) throws IOException {
+        getJobs();
+    }
+
+}
