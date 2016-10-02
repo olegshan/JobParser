3d2390bef78ec472f58342c339f632fafda71f26
Oleg Shankovskyi <olegshan@gmail.com>
2016-10-02 11:18:10 +0300

    Revert: equals() and hashCode() removed

--- src/main/java/com/olegshan/entity/Job.java
+++ src/main/java/com/olegshan/entity/Job.java
@@ -79,33 +79,6 @@ public class Job {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Job job = (Job) o;
-
-        if (title != null ? !title.equals(job.title) : job.title != null) return false;
-        if (description != null ? !description.equals(job.description) : job.description != null) return false;
-        if (company != null ? !company.equals(job.company) : job.company != null) return false;
-        if (source != null ? !source.equals(job.source) : job.source != null) return false;
-        if (url != null ? !url.equals(job.url) : job.url != null) return false;
-        return date != null ? date.equals(job.date) : job.date == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = title != null ? title.hashCode() : 0;
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        result = 31 * result + (company != null ? company.hashCode() : 0);
-        result = 31 * result + (source != null ? source.hashCode() : 0);
-        result = 31 * result + (url != null ? url.hashCode() : 0);
-        result = 31 * result + (date != null ? date.hashCode() : 0);
-        return result;
-    }
-
-    @Override
     public String toString() {
         return "Title: " + title + "\n" +
                 "Description: " + description + "\n" +
