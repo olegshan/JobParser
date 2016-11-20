d3f06aadf494ea32437b671f648fa716a26fbdf9
Oleg Shankovskyi <olegshan@gmail.com>
2016-11-20 12:27:58 +0200

    JobService tests improved

--- src/test/java/com/olegshan/service/JobServiceTest.java
+++ src/test/java/com/olegshan/service/JobServiceTest.java
@@ -7,17 +7,24 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class JobServiceTest extends AbstractTest {
 
-    private static final String JOB_ONE_URL = "http://somesite.ua/company1/vacancy1";
+    private static final String JOB_URL = "http://somesite.ua/company/vacancy";
+    private static final int CURRENT_PAGE = 1;
+    private static final int PAGE_SIZE = 5;
 
     @Autowired
     private JobService jobService;
@@ -26,55 +33,63 @@ public class JobServiceTest extends AbstractTest {
 
     @Before
     public void setUp() throws Exception {
-        Job job = new Job("Java Developer", "Experienced Java developer needed", "Luxoft", "Dou.ua",
-                JOB_ONE_URL, LocalDateTime.now(ZoneId.of("Europe/Athens")));
-        jobService.save(job);
+        Job job;
+        Random random = new Random(20);
+        for (int i = 0; i < 10; i++) {
+            //jobs are saved into database with random dates
+            job = new Job("Title" + i, "Description" + i, "Company" + i, "Site" + i, JOB_URL + i,
+                    LocalDateTime.now(ZoneId.of("Europe/Athens")).minusDays(random.nextInt()));
+            jobService.save(job);
+        }
     }
 
     @Test
-    public void jobInSetUpMethodWasSavedProperly() throws Exception {
-        Job job = jobRepository.findOne(JOB_ONE_URL);
-        assertNotNull("The job with url " + JOB_ONE_URL + " should be present in database", job);
-        assertEquals("Java Developer", job.getTitle());
-        assertEquals("Experienced Java developer needed", job.getDescription());
-        assertEquals("Luxoft", job.getCompany());
-        assertEquals("Dou.ua", job.getSource());
-        assertEquals(LocalDate.now(), job.getDate().toLocalDate());
-        assertEquals("There should be only one element in the database", jobRepository.findAll().size(), 1);
+    public void jobsInSetUpMethodWereSaved() throws Exception {
+        assertEquals("There should be 10 elements in the database", jobRepository.findAll().size(), 10);
     }
 
     @Test
-    public void savingOfNewJobWithTheSameUrlAndDifferenDateUpdatesExistingJob() throws Exception {
-        Job job = new Job("Java Programmer", "Experienced Java developer needed", "Epam", "Dou.ua",
-                JOB_ONE_URL, LocalDateTime.now(ZoneId.of("Europe/Athens")).minusDays(3));
+    public void savingOfNewJobWithTheSameUrlAndDifferentDateUpdatesExistingJob() throws Exception {
+        Job job = jobRepository.findOne(JOB_URL + 5);
+        assertEquals("Title5", job.getTitle());
+        LocalDateTime newDate = job.getDate().minusDays(1);
+        job.setDate(newDate);
+        job.setTitle("New title");
         jobService.save(job);
-        job = jobRepository.findOne(JOB_ONE_URL);
-        assertEquals("Java Programmer", job.getTitle());
-        assertEquals("Experienced Java developer needed", job.getDescription());
-        assertEquals("Epam", job.getCompany());
-        assertEquals("Dou.ua", job.getSource());
-        assertEquals("newJob's date should be 3 days earlier from now",
-                LocalDate.now().minusDays(3), job.getDate().toLocalDate());
-        assertEquals("There should be only one element in the database after updating",
-                jobRepository.findAll().size(), 1);
+
+        job = jobRepository.findOne(JOB_URL + 5);
+        assertEquals("New title", job.getTitle());
+        assertEquals(newDate, job.getDate());
+        assertEquals("There should be still 10 elements in the database after updating",
+                jobRepository.findAll().size(), 10);
     }
 
     @Test
     public void savingOfJobWithTheSameUrlAndSameDateDoesNotUpdateExistingJob() throws Exception {
-        Job job = new Job("Java Guru", "Very experienced", "GlobalLogic", "Work.ua",
-                JOB_ONE_URL, LocalDateTime.now(ZoneId.of("Europe/Athens")));
+        Job job = jobRepository.findOne(JOB_URL + 7);
+        assertEquals("Title7", job.getTitle());
+        job.setTitle("New title");
         jobService.save(job);
-        job = jobRepository.findOne(JOB_ONE_URL);
-        assertEquals("Java Developer", job.getTitle());
-        assertEquals("Experienced Java developer needed", job.getDescription());
-        assertEquals("Luxoft", job.getCompany());
-        assertEquals("Dou.ua", job.getSource());
-        assertEquals(LocalDate.now(), job.getDate().toLocalDate());
-        assertEquals("There should be only one element in the database", jobRepository.findAll().size(), 1);
+
+        job = jobRepository.findOne(JOB_URL + 7);
+        assertEquals("Title7", job.getTitle());
+        assertEquals("There should be still 10 elements in the database", jobRepository.findAll().size(), 10);
+    }
+
+    @Test
+    public void jobsAreRetrievedFromDatabaseSortedByDateInDescendingOrder() throws Exception {
+        Page<Job> jobs = jobService.getJobs(new PageRequest(CURRENT_PAGE, PAGE_SIZE, Sort.Direction.DESC, "date"));
+        assertTrue("The jobs should be sorted from new to old", isSorted(jobs));
     }
 
     @After
     public void tearDown() throws Exception {
         jobRepository.deleteAll();
+    }
+
+    private boolean isSorted(Page<Job> page) {
+        List<Job> list = page.getContent();
+        return IntStream.range(0, PAGE_SIZE - 1).allMatch(i -> list.get(i).getDate()
+                .compareTo(list.get(i + 1).getDate()) > 0);
     }
 }
