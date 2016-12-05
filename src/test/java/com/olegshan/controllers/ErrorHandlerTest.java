978804944d370b09d780274de93078d0d7c2d871
Oleg Shankovskyi <olegshan@gmail.com>
2016-12-05 23:00:46 +0200

    ErrorHandler test added

---
+++ src/test/java/com/olegshan/controllers/ErrorHandlerTest.java
@@ -0,0 +1,40 @@
+package com.olegshan.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ErrorHandlerTest {
+
+    @Mock
+    ParseController parseController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(parseController)
+                .setControllerAdvice(new ErrorHandler())
+                .build();
+    }
+
+    @Test
+    public void unexpectedExceptionsAreCaught() throws Exception {
+
+        when(parseController.about()).thenThrow(new RuntimeException("Unexpected exception"));
+
+        mockMvc.perform(get("/about"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("exception"))
+                .andExpect(model().attribute("errorMessage", "Unexpected exception"));
+    }
+}
