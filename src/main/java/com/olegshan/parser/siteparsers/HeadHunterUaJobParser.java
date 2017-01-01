3ff40e76b1c31fec637e726cfa11aa886936b851
Oleg Shankovskyi <olegshan@gmail.com>
2017-01-02 00:37:09 +0200

    Issues of parsing in January HeadHunter jobs of last December fixed

--- src/main/java/com/olegshan/parser/siteparsers/HeadHunterUaJobParser.java
+++ src/main/java/com/olegshan/parser/siteparsers/HeadHunterUaJobParser.java
@@ -20,10 +20,21 @@ public class HeadHunterUaJobParser extends JobParser {
         String[] dateParts = dateLine.split(jobSite.getSplit());
         MonthsTools.removeZero(dateParts);
 
-        int year = LocalDate.now(ZoneId.of("Europe/Athens")).getYear();
-        int month = MonthsTools.MONTHS.get(dateParts[1].toLowerCase());
         int day = parseInt(dateParts[0]);
+        int month = MonthsTools.MONTHS.get(dateParts[1].toLowerCase());
+        int year = getYear(month);
 
         return LocalDate.of(year, month, day).atTime(getTime());
+    }
+
+    //in case we parse in January jobs of last December
+    private int getYear(int month) {
+        int year;
+        if (month > LocalDate.now(ZoneId.of("Europe/Athens")).getMonthValue()) {
+            year = LocalDate.now().getYear() - 1;
+        } else {
+            year = LocalDate.now(ZoneId.of("Europe/Athens")).getYear();
+        }
+        return year;
     }
 }
