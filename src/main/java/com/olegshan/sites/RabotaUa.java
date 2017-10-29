e3dfb3f2fa2dc87651961ba2687888c39b694e03
olegshan <olegshan@gmail.com>
2017-10-29 15:12:08 +0200

    Rabota.ua url prefix fixed

--- src/main/java/com/olegshan/sites/RabotaUa.java
+++ src/main/java/com/olegshan/sites/RabotaUa.java
@@ -9,7 +9,7 @@ public class RabotaUa implements JobSite {
 
 	private static final String SITE_NAME        = "Rabota.ua";
 	private static final String SITE_URL         = "https://rabota.ua/zapros/java/%D0%BA%D0%B8%D0%B5%D0%B2";
-	private static final String URL_PREFIX       = "http://rabota.ua";
+	private static final String URL_PREFIX       = "https://rabota.ua";
 	private static final String SPLIT            = "";
 	private static final Holder JOB_BOX          = Holder.of("class", "f-vacancylist-vacancyblock");
 	private static final Holder TITLE_BOX        = Holder.of("class", "fd-beefy-gunso");
